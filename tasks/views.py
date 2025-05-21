c8104084c48209af3734685557752cf59e0378f4
Moussa-codeur <correamoussa469@gmail.com>
2025-05-21 10:45:25 +0200

    Création d'un fichier serializers.py et mise en place dema views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# tasks/views.py
+from rest_framework import generics, permissions
+from rest_framework.authentication import TokenAuthentication
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
